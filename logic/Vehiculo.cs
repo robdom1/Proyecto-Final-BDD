38ba9ea4c09ec52d8883eccabb821783cf2f9cbe
JOHN HAROLD PEÑA ACOSTA <20180767@ce.pucmm.edu.do>
2020-11-26 22:13:27 -0400

    UPDATE CLASE VEHICULO

--- logic/Vehiculo.cs
+++ logic/Vehiculo.cs
@@ -19,7 +19,7 @@ namespace Proyecto_Final_BDD.logic
         private string motor;
         private string transmision;
         private string traccion;
-        private List<string> accesorios;
+        private string accesorios;
         private string exterior;
         private string interior;
         private string combustible;
@@ -29,7 +29,7 @@ namespace Proyecto_Final_BDD.logic
 
         //Constructor
         public Vehiculo(string codVehiculo, string marca, string modelo, string tipo, int ano, string kilometraje, 
-                        string estado, string motor, string transmision, string traccion, List<string> accesorios, 
+                        string estado, string motor, string transmision, string traccion, string accesorios, 
                         string exterior, string interior, string combustible, int puertas, int pasajeros)
         {
             this.codVehiculo = codVehiculo;
@@ -54,6 +54,8 @@ namespace Proyecto_Final_BDD.logic
 
         //Gets y Sets
         public string CodVehiculo { get => codVehiculo; set => codVehiculo = value; }
+
+        public string Accesorios { get => accesorios; set => accesorios = value; }
         public string Marca { get => marca; set => marca = value; }
         public string Modelo { get => modelo; set => modelo = value; }
         public string Tipo { get => tipo; set => tipo = value; }
