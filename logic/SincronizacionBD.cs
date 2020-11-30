80f088e1609bdeb019b7707f2895a984735e2a40
JOHN HAROLD PEÑA ACOSTA <20180767@ce.pucmm.edu.do>
2020-11-29 23:55:25 -0400

    SISTEMA BD INCLUIDO

---
+++ logic/SincronizacionBD.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Windows.Forms;
+
+namespace Proyecto_Final_BDD.logic
+{
+    class SincronizacionBD
+    {
+
+        SqlConnection conectarbd;
+        SqlCommand cmd;
+        SqlDataReader read;
+
+        public void Conexionbd()
+        {
+            conectarbd = new SqlConnection("Data Source=.;Initial Catalog=PROYECTO;Integrated Security=True"); //esto es lo que vas a cambiar robert
+            string mensaje;
+            try
+            {
+                conectarbd.Open();
+
+                mensaje = "Conexion abierta";
+                Console.WriteLine(mensaje);
+               
+            }
+            catch (Exception ex)
+            {
+                mensaje = "Error al intentar conectarse, " +ex.Message;
+                Console.WriteLine(mensaje);
+             
+            }
+
+        }
+
+
+
+       
+        //funciones de insertar y modificar en la BD
+
+
+        public void cerrar()
+        {
+            conectarbd.Close();
+
+        }
+
+    }
+}
